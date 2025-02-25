4464dfe164b7df584109e0c31d0e0a9f755e2313
unielkarpavicius <unielkarpavicius@fei.edu.br>
2025-02-25 19:01:04 +0000

    preparação lab03

--- ldde.c
+++ ldde.c
@@ -59,11 +59,48 @@ void inserir(Lista *ldde, int valor){
     ldde->qtde++;
 }
 
-void remover(){
-    return;
+void remover(Lista *ldde, int valor){
+    if (ldde == NULL || ldde->primeira == NULL) { // Verifica se a lista está vazia
+        printf("A lista está vazia.\n");
+        return;
+    }
+    Celula *anterior = NULL;
+    Celula *atual = ldde->primeira;
+    // Busca o celula a ser removido
+    while (atual != NULL && atual->valor < valor) {                                                             
+        anterior = atual;                                    
+        atual = atual->proximo;
+    }
+    // Se não encontrou o valor na lista
+    if (atual == NULL || atual->valor != valor) {
+        printf("Valor %d não encontrado na lista.\n", valor);
+        return;
+    }
+    Celula *proximo = atual->proximo; // Obtém o próximo celula
+    // Se for o primeiro celula da lista
+    if (anterior == NULL) {
+        ldde->primeira = proximo;
+        proximo->anterior = NULL;
+    }
+    // Se for um celula no final
+    else if (proximo == NULL) {
+        anterior->proximo = NULL; // O anterior passa a ser o último
+    } 
+    //Remover do meio
+    else {
+        anterior->proximo = proximo;
+        proximo->anterior = anterior;
+    }
+
+    free(atual); // Libera a memória
+    ldde->qtde--;
 }
 
 void imprimir(Lista *ldde){
+    if (ldde == NULL || ldde->primeira == NULL) { // Verifica se a lista está vazia
+        printf("A lista está vazia.\n");
+        return;
+    }
     Celula *atual = ldde->primeira;
 
     printf("[");
@@ -75,12 +112,41 @@ void imprimir(Lista *ldde){
 
 }
 
-void imprmir_reverso(){
-    return;
+void imprmir_reverso(Lista *ldde){
+    if (ldde == NULL || ldde->primeira == NULL) { // Verifica se a lista está vazia
+        printf("A lista está vazia.\n");
+        return;
+    }
+    Celula *atual = ldde->primeira;
+    // Percorre a lista até o último celula
+    while (atual->proximo != NULL) {
+        atual = atual->proximo;
+    }
+    // Agora `atual` está no último celula, então percorremos de trás para frente
+    printf("[");
+    while (atual != NULL) {
+        printf(" %d ", atual->valor);
+        atual = atual->anterior; 
+    }
+    printf("] - %d \n", ldde->qtde);
 }
 
-void buscar(){
-    return;
+void buscar(Lista *ldde, int valor){
+    if (ldde == NULL || ldde->primeira == NULL) { // Verifica se a lista está vazia
+        printf("A lista está vazia.\n");
+        return;
+    }
+    Celula *atual = ldde->primeira;
+    int posicao = 0;
+    while (atual != NULL) {
+        if (atual->valor == valor) { // Se encontrou o valor
+            printf("O número %d está na posição %d.\n", valor, posicao);
+            return;
+        }
+        atual = atual->proximo;
+        posicao++;
+    }
+    printf("O número %d não está na lista.\n", valor); // Se o valor não foi encontrado
 }
 
 int main(){
@@ -95,6 +161,14 @@ int main(){
     inserir(ldde, 70);
     imprimir(ldde);
     inserir(ldde, 50);
+    imprimir(ldde);
+
+    //imprmir_reverso(ldde);
+
+    buscar(ldde, 56);
+    remover(ldde, 30);
+    remover(ldde, 56);
+    remover(ldde, 70);
     imprimir(ldde);
     
     return 0;
