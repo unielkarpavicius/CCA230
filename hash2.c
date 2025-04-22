0d723fb766a4c44aa699d319049e9d170fb7e81e
unielkarpavicius <unielkarpavicius@fei.edu.br>
2025-04-22 19:10:30 +0000

    preparação lab 09

--- hash2.c
+++ hash2.c
@@ -20,7 +20,7 @@ typedef struct Hash{
 Hash* inicializa(){
     Hash* hash = malloc(sizeof(Hash));
     for (int i = 0; i < LEN; i++){
-        Lista* lista = malloc(sizeof(lista));
+        Lista* lista = malloc(sizeof(Lista));  
         lista->inicio = NULL;
         lista->qtde = 0;
         hash->table[i] = lista;
@@ -48,32 +48,53 @@ void inserir(Hash* hash, int valor){
 int buscar(Hash* hash, int valor){
     int pos = valor % LEN;
 
-    Celula* anterior = NULL;
     Celula* atual = hash->table[pos]->inicio;
-    while(atual != NULL && atual->valor != valor){
-        anterior = atual;
+    while(atual != NULL){
+        if(atual->valor == valor){
+            return valor;
+        }
         atual = atual->proxima;
     }
-    if (atual != NULL){
-        return valor;
-    }
+
     return -1;
 }
 
 int remover(Hash* hash, int valor){
+    int pos = valor % LEN;
+    Celula* atual = hash->table[pos]->inicio;
+    Celula* anterior = NULL;
 
+    while (atual != NULL && atual->valor != valor) {
+        anterior = atual;
+        atual = atual->proxima;
+    }
+
+    if (atual == NULL) { // valor não encontrado
+        return 0; 
+    }
+
+    if (anterior == NULL) {         // Remover do início
+        hash->table[pos]->inicio = atual->proxima;
+    } else {
+        anterior->proxima = atual->proxima;
+    }
+
+    free(atual);
+    hash->table[pos]->qtde--;
+
+    return 1; 
 }
 
 void imprimir(Hash* hash){
     for(int i =0; i < LEN; i++){
+        printf("[%d]: ", i);
         Celula* atual = hash->table[i]->inicio;
         while(atual != NULL){
-            printf("%d ", atual->valor);
+            printf("%d -> ", atual->valor);
             atual = atual->proxima;
         }
-        printf("\n");
+        printf("NULL\n");
     }
-
 }
 
 int main(){
@@ -87,10 +108,18 @@ int main(){
 
     int x = buscar(hash, 5);
     if (x >= 0){
-        printf("Valor encotrado: %d\n", x);
+        printf("Valor encontrado: %d\n", x);
     } else {
-        printf("Valor não encotrado\n");
+        printf("Valor não encontrado\n");
     }
+
+    if (remover(hash, 5)){
+        printf("Removido com sucesso!\n");
+    } else {
+        printf("Valor não encontrado para remoção\n");
+    }
+
+    imprimir(hash);
 
     return 0;
 }
